17c1eca36a5b912f4c9174c2aa184d7e7afd5cd5
Valeria Protsan <valeriyapro@gmail.com>
2018-06-29 17:54:33 -0400

    lowest payment

--- PayingDebtoffinaYear.py
+++ PayingDebtoffinaYear.py
@@ -55,23 +55,58 @@
 # Lowest Payment: 440
 
 #Test Case 3
+# balance = 19;
+# annualInterestRate = 0.18
+########################################################################
+
+
+########################################################################
+# minimumFixedMonthlyPayment = 10
+# total = balance
+# if minimumFixedMonthlyPayment * 12 > balance:
+#     print("Lowest Payment: " + str(minimumFixedMonthlyPayment))
+#
+# while balance > 0:
+#     balance = total
+#     minimumFixedMonthlyPayment += 10
+#     for el in range(12):
+#         monthlyInterestRate = annualInterestRate / 12.0
+#         monthlyUnpaidBalance = balance - minimumFixedMonthlyPayment
+#         balance = monthlyUnpaidBalance + (monthlyInterestRate * monthlyUnpaidBalance)
+# print("Lowest Payment: " + str(minimumFixedMonthlyPayment))
+########################################################################
+
+
+########################################################################
+#to find the smallest monthly payment to the cent
 balance = 19;
 annualInterestRate = 0.18
-########################################################################
+#
+#
+monthlyInterestRate = annualInterestRate / 12.0
+low = balance / 12
+high = (balance * ((1 + monthlyInterestRate)**12)) / 12.0
+guess = (high + low) / 2
+balLeft = balance
 
+while balLeft != 0:
+    balLeft = balance
 
-########################################################################
-minimumFixedMonthlyPayment = 10
-total = balance
-if minimumFixedMonthlyPayment * 12 > balance:
-    print("Lowest Payment: " + str(minimumFixedMonthlyPayment))
-
-while balance > 0:
-    balance = total
-    minimumFixedMonthlyPayment += 10
     for el in range(12):
-        monthlyInterestRate = annualInterestRate / 12.0
-        monthlyUnpaidBalance = balance - minimumFixedMonthlyPayment
-        balance = monthlyUnpaidBalance + (monthlyInterestRate * monthlyUnpaidBalance)
-print("Lowest Payment: " + str(minimumFixedMonthlyPayment))
+        monthlyUnpaidBalance = balLeft - guess
+        balLeft = round(monthlyUnpaidBalance + (monthlyInterestRate * monthlyUnpaidBalance), 2)
+    # print("balance left after loop: " + str(balLeft))
+
+    if balLeft == 0:
+        break
+    elif balLeft > 0:
+        low = guess
+        # print("NEW low bound " + str(low))
+    elif balLeft < 0:
+        high = guess
+        # print("NEW high bound " + str(high))
+    guess = (high + low) / 2
+    # print("new guess is : " + str(guess))
+print("Lowest Payment: " + str(round(low, 2)))
+
 ########################################################################
